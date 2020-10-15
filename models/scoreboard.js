193fd88656a7044d7f62525735bde6f2740e9f80
mbrazze <bryan.beale88@gmail.com>
2020-10-15 14:46:19 +0100

    functions

--- models/scoreboard.js
+++ models/scoreboard.js
@@ -1,3 +1,23 @@
 const { query } = require("../db/index");
 
-module.exports = {};
+const readStatement = `SELECT * from scoreboard;`;
+
+async function displayScores() {
+  const result = await query(readStatement);
+  return result;
+};
+
+const addScoresStatement = `INSERT INTO scoreboard 
+(date,game,team1_score,team2_score,team3_score,team4_score)
+VALUES 
+($1, $2, $3, $4, $5, $6)`
+
+
+async function displayScores() {
+  const result = await query(readStatement);
+  return result;
+};
+
+module.exports = {
+    displayScores,
+};
--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -1,4 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
+router.get("/", async function (req, res){
+    const result = await displayScores();
+    console.log(result)
+    res.json({sucess: true, payload: result});
+});
+
+router.post("/", async function(req, res){
+    const {date, game, team1_score, team2_score, team3_score, team4_score} = req.body;
+    const result = await addNewScore(date, game, team1_score, team2_score, team3_score, team4_score);
+    res.json({sucess: true, payload: result})
+});
+
+
+
 module.exports = router;
